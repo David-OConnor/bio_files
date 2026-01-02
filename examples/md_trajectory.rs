d5b5265d818805283fd3d8e8bcfee503534e4449
David O'Connor <the_alchemist@fastmail.com>
2026-01-02 18:23:18 -0500

    More ex work

--- examples/md_trajectory.rs
+++ examples/md_trajectory.rs
@@ -14,7 +14,10 @@ fn main() {
     let traj = DcdTrajectory::load_xtc(path_xtc).unwrap();
 
     for frame in &traj.frames {
-        println!("Time: {} unit cell: {:?}", frame.time, frame.unit_cell);
+        println!(
+            "Time: {:.1} fs unit cell: {:?}",
+            frame.time, frame.unit_cell
+        );
         println!("Positions:");
         for posit in &frame.atom_posits {
             // ...
@@ -24,4 +27,15 @@ fn main() {
     // To save:
     traj.save(path_dcd).unwrap();
     traj.save_xtc(path_xtc).unwrap();
+
+    // If using the `dynamics` lib:
+    // let snapshots = dynamics::load_snapshots_from_file(path_dcd).unwrap();
+    // Or
+    // let snaps = dynamics::Snapshot::from_dcd(&traj);
+
+    // To convert a Snapshot:
+    // snap.to_dec(cell, false), // Unit cell, if we should write water atoms.
+
+    // You will likely use a similar adapter for your application or library's native snapshot/frame
+    // format.
 }
